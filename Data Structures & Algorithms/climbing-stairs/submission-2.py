dd8d62073886fa5d70c194f15a86ce39d32beb53
ggbond06 <167107064+ggbond06@users.noreply.github.com>
2026-09-21 12:27:01 -0400

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        dp = [0] * n
+
+        if n == 1:
+            return 1
+
+        dp[0] = 1
+        dp[1] = 2
+
+        for i in range(2, n):
+            dp[i] = dp[i - 2] + dp[i - 1]
+
+        return dp[-1]
